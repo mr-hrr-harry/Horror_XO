cbe12260225e443ac0f3811e0249c2e69c94df65
M A Harish J <mr.horror3737@gmail.com>
2023-03-26 01:46:43 +0530

    js half cmmt

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 let mat=[[0,0,0],
          [0,0,0],
          [0,0,0]];
+var flag=0, ctr=0;
 
 function xORo(event){
+
+    console.log(ctr);
+
     var id = event.target.id;
 
-    for(i=0, rows; i<rows[i].length; i++){
-        console.log(mt[i][i]);
+    const cell = document.getElementById(id);
+    const row = parseInt(id[0]);
+    const col = parseInt(id[1]);
+
+    // const mainBg = document.getElementById("main");
+    // if(flag==0){
+    //     //mainBg.
+    // }
+
+    if(cell.innerHTML===""){
+        if(flag==0){
+            cell.className="xAttr";
+            flag=1;
+            ctr++;
+            cell.innerHTML="X";
+        }
+        else{
+            cell.innerHTML="O";
+            cell.className="oAttr";
+            flag=0;
+            ctr++;
+        }
     }
+    else{
+        alert("Fill on empty boxes");
+    }
+
+    console.log("clicked");
+
+    if(ctr==9){
+        alert("Oops! Match Draw 👻");
+        prompt("Do you like this game ?");
+    }
+
 }
