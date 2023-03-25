3aede3ff59fd64954adb8c1708ca39b637e97916
M A Harish J <mr.horror3737@gmail.com>
2023-03-25 22:35:25 +0530

    index cmmt

---
+++ script.js
@@ -0,0 +1,11 @@
+let mat=[[0,0,0],
+         [0,0,0],
+         [0,0,0]];
+
+function xORo(event){
+    var id = event.target.id;
+
+    for(i=0, rows; i<rows[i].length; i++){
+        console.log(mt[i][i]);
+    }
+}
